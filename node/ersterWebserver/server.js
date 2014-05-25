8ac5b5bdbb303963d9bc4ccf1ee1aa2bae1fa6ed
TobiasNickel <ross40k@googlemail.com>
2014-05-25 12:44:12 +0200

    server.js

---
+++ node/ersterWebserver/server.js
@@ -0,0 +1,34 @@
+var http = require('http');
+
+var bestellungen=[];//{"product":""}
+
+http.createServer(function (request, response) {
+	response.writeHead(200, {'Content-Type': 'text/plain'});
+	console.log(request.url);
+	// /user/10
+	var urlPieces=request.url.split("/");
+	switch(urlPieces[1]){
+		case "bestellung": 
+			response.end(
+				JSON.stringify(
+					bestellungen[urlPieces[2]]
+				)
+			);
+			break;
+		case "createBestellung":
+			bestellungen.push({product:urlPieces[2]});
+			response.end(JSON.stringify(true));
+			break;
+		case "alleBestellungen.json":
+			response.end(
+				JSON.stringify(
+					bestellungen
+				)
+			);
+			break;
+		default:
+			response.end('Hello World\n');
+	}
+}).listen(1337);
+
+console.log('Server running at http://127.0.0.1:1337/');
